d7122eebf40bfa47281c0c705c8f6c2f047084b5
Junaid Majumdar <jmajumdar@agero.com>
2026-06-08 13:38:43 -0400

    adding netskope enroll script

--- SANDBOX/terraform/main.tf
+++ SANDBOX/terraform/main.tf
@@ -8,13 +8,3 @@ resource "aws_budgets_budget" "total_cost" {
   time_unit         = "MONTHLY"
 }
 
-resource "terraform_data" "netskope_provisioning" {
-  # Forces this script to run exactly once when an account passes pipeline changes
-  triggers_replace = [
-    var.account_id
-  ]
-
-  provisioner "local-exec" {
-    command = "python3 ${path.module}/netskope_customization.py"
-  }
-}
